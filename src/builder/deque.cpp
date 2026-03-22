ffa638b774368d1ee49c6f5b391854348e46a43b
Jnyfah <jnyfaah@gmail.com>
2026-03-22 13:56:13 +0100

    lock free deque no growth yet

--- src/builder/deque.cpp
+++ src/builder/deque.cpp
@@ -1,8 +1,10 @@
 module;
 
+#include <atomic>
 #include <cstddef>
 #include <memory>
-#include <stdexcept>
+#include <optional>
+#include <utility>
 
 module builder:deque;
 
@@ -12,68 +14,103 @@ class LockFreeDeque
   public:
     LockFreeDeque() : data(std::make_unique<T[]>(capacity)) {}
 
-    auto push_back(const T& item) -> bool
+    auto push_back(T&& item) -> void
     {
-        if (size == capacity)
+        auto f = front.load(std::memory_order_relaxed);
+        auto b = back.load(std::memory_order_acquire);
+
+        if (b - f >= capacity)
         {
-            grow();
+            return; // todo
         }
 
-        const std::size_t index = (front + size) % capacity;
-        data[index] = item;
-        ++size;
-        return true;
+        data[b & mask] = std::move(item);
+        std::atomic_thread_fence(std::memory_order_release);
+        back.store(b + 1, std::memory_order_relaxed);
     }
 
-    auto pop_back() -> T
+    auto pop_back() -> std::optional<T>
     {
-        if (size == 0)
+        auto b = back.load(std::memory_order_acquire);
+
+        if (b == 0)
         {
-            throw std::out_of_range("deque is empty");
+            return std::nullopt; // guard against underflow as b cant be -1
         }
 
-        const std::size_t index = (front + size - 1) % capacity;
-        T value = data[index];
-        --size;
-        return value;
-    }
+        b = b - 1;
+        back.store(b, std::memory_order_relaxed);
+        std::atomic_thread_fence(std::memory_order_seq_cst);
+        auto f = front.load(std::memory_order_relaxed);
 
-    auto pop_front() -> T
-    {
-        if (size == 0)
+        if (f > b)
         {
-            throw std::out_of_range("deque is empty");
+            // if the front has advanced past the back it means that the queue is empty
+            back.store(f, std::memory_order_relaxed);
+            return std::nullopt;
         }
 
-        T value = data[front];
-        front = (front + 1) % capacity;
-        --size;
-        return value;
+        T item = std::move(data[b & mask]);
+
+        // if there is one item left, CAS to see who wins the race to pick the last item
+        if (f == b)
+        {
+            // am I the one to chnage front ??
+            if (!front.compare_exchange_strong(f, f + 1, std::memory_order_seq_cst, std::memory_order_relaxed))
+            {
+                // if no then update the back to b
+                back.store(f, std::memory_order_relaxed);
+                return std::nullopt;
+            }
+
+            // if yes then update back
+            back.store(f + 1, std::memory_order_relaxed);
+        }
+        return item;
     }
 
-    [[nodiscard]] auto empty() const -> bool { return size == 0; }
+    auto pop_front() -> std::optional<T>
+    {
+        auto f = front.load(std::memory_order_relaxed);
+        std::atomic_thread_fence(std::memory_order_seq_cst);
 
-    [[nodiscard]] auto Size() const -> std::size_t { return size; }
+        auto b = back.load(std::memory_order_relaxed);
+
+        if (f >= b)
+        {
+            return std::nullopt;
+        }
+
+        T item = std::move(data[f & mask]);
+
+        if (!front.compare_exchange_strong(f, f + 1, std::memory_order_seq_cst, std::memory_order_relaxed))
+        {
+            return std::nullopt;
+        }
+
+        return item;
+    }
+
+    [[nodiscard]] auto empty() const -> bool
+    {
+        const size_t f = front.load(std::memory_order_acquire);
+        const size_t b = back.load(std::memory_order_acquire);
+
+        return f >= b;
+    }
+
+    [[nodiscard]] auto size() const -> std::size_t
+    {
+
+        const size_t f = front.load(std::memory_order_acquire);
+        const size_t b = back.load(std::memory_order_acquire);
+        return (b >= f) ? (b - f) : 0;
+    }
 
   private:
-    // NOLINTNEXTLINE(cppcoreguidelines-avoid-c-arrays)
     std::unique_ptr<T[]> data;
-    std::size_t capacity{16};
-    std::size_t front{0};
-    std::size_t size{0};
-
-    void grow()
-    {
-        std::size_t new_capacity = capacity * 2;
-        auto new_data = std::make_unique<T[]>(new_capacity);
-
-        for (std::size_t i = 0; i < size; ++i)
-        {
-            new_data[i] = data[(front + i) % capacity];
-        }
-
-        data = std::move(new_data);
-        capacity = new_capacity;
-        front = 0;
-    }
+    alignas(64) std::atomic_size_t front{0};
+    alignas(64) std::atomic_size_t back{0};
+    const std::size_t capacity{16};
+    const std::size_t mask = capacity - 1;
 };
